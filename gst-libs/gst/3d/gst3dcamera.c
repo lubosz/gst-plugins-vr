f2c4e74a4d1345ab43079498941ad62e438b1675
Lubosz Sarnecki <lubosz.sarnecki@collabora.co.uk>
2016-05-22 16:16:30 +0200

    project: Run gst-indent

--- gst-libs/gst/3d/gst3dcamera.c
+++ gst-libs/gst/3d/gst3dcamera.c
@@ -19,7 +19,7 @@
  */
 
 #ifdef HAVE_CONFIG_H
-  #include "config.h"
+#include "config.h"
 #endif
 
 #include <string.h>
@@ -38,7 +38,9 @@ G_DEFINE_TYPE_WITH_CODE (Gst3DCamera, gst_3d_camera, GST_TYPE_OBJECT,
     GST_DEBUG_CATEGORY_INIT (gst_3d_camera_debug, "3dcamera", 0, "camera"));
 
 
-static void init_hmd(Gst3DCamera * self) {
+static void
+init_hmd (Gst3DCamera * self)
+{
   self->hmd_context = ohmd_ctx_create ();
   int num_devices = ohmd_ctx_probe (self->hmd_context);
   if (num_devices < 0) {
@@ -49,12 +51,15 @@ static void init_hmd(Gst3DCamera * self) {
 
   GST_DEBUG ("We have %d deivces.\n", num_devices);
 
-	for(int i = 0; i < num_devices; i++){
-		GST_DEBUG("device %d", i);
-		GST_DEBUG("  vendor:  %s", ohmd_list_gets(self->hmd_context, i, OHMD_VENDOR));
-		GST_DEBUG("  product: %s", ohmd_list_gets(self->hmd_context, i, OHMD_PRODUCT));
-		GST_DEBUG("  path:    %s", ohmd_list_gets(self->hmd_context, i, OHMD_PATH));
-	}
+  for (int i = 0; i < num_devices; i++) {
+    GST_DEBUG ("device %d", i);
+    GST_DEBUG ("  vendor:  %s", ohmd_list_gets (self->hmd_context, i,
+            OHMD_VENDOR));
+    GST_DEBUG ("  product: %s", ohmd_list_gets (self->hmd_context, i,
+            OHMD_PRODUCT));
+    GST_DEBUG ("  path:    %s", ohmd_list_gets (self->hmd_context, i,
+            OHMD_PATH));
+  }
 
   self->device = ohmd_list_open_device (self->hmd_context, 0);
 
@@ -63,79 +68,87 @@ static void init_hmd(Gst3DCamera * self) {
         ohmd_ctx_get_error (self->hmd_context));
     return;
   }
-  
+
   int screen_width;
   int screen_height;
-  
+
   float inch = 2.54;
-  
-  ohmd_device_geti (self->device, OHMD_SCREEN_HORIZONTAL_RESOLUTION, &screen_width);
-  ohmd_device_geti (self->device, OHMD_SCREEN_VERTICAL_RESOLUTION, &screen_height);
-  
-  GST_DEBUG("HMD screen resolution: %dx%d", screen_width, screen_height);
-  
+
+  ohmd_device_geti (self->device, OHMD_SCREEN_HORIZONTAL_RESOLUTION,
+      &screen_width);
+  ohmd_device_geti (self->device, OHMD_SCREEN_VERTICAL_RESOLUTION,
+      &screen_height);
+
+  GST_DEBUG ("HMD screen resolution: %dx%d", screen_width, screen_height);
+
   float screen_width_physical;
   float screen_height_physical;
-  
-  ohmd_device_getf (self->device, OHMD_SCREEN_HORIZONTAL_SIZE, &screen_width_physical);
-  ohmd_device_getf (self->device, OHMD_SCREEN_VERTICAL_SIZE, &screen_height_physical);
-  
-  GST_DEBUG("Physical HMD screen dimensions: %.3fx%.3fcm",
-    screen_width_physical * 100.0, screen_height_physical * 100.0);
 
-  float x_pixels_per_cm = (float) screen_width / (screen_width_physical * 100.0);
-  float y_pixels_per_cm = (float) screen_height / (screen_height_physical * 100.0);
+  ohmd_device_getf (self->device, OHMD_SCREEN_HORIZONTAL_SIZE,
+      &screen_width_physical);
+  ohmd_device_getf (self->device, OHMD_SCREEN_VERTICAL_SIZE,
+      &screen_height_physical);
 
-  GST_DEBUG("HMD DPI %f x %f", x_pixels_per_cm / inch, y_pixels_per_cm / inch);
+  GST_DEBUG ("Physical HMD screen dimensions: %.3fx%.3fcm",
+      screen_width_physical * 100.0, screen_height_physical * 100.0);
+
+  float x_pixels_per_cm =
+      (float) screen_width / (screen_width_physical * 100.0);
+  float y_pixels_per_cm =
+      (float) screen_height / (screen_height_physical * 100.0);
+
+  GST_DEBUG ("HMD DPI %f x %f", x_pixels_per_cm / inch, y_pixels_per_cm / inch);
 
   float lens_x_separation;
   float lens_y_position;
-  
-  ohmd_device_getf (self->device, OHMD_LENS_HORIZONTAL_SEPARATION, &lens_x_separation);
-  ohmd_device_getf (self->device, OHMD_LENS_VERTICAL_POSITION, &lens_y_position);
-  
-  GST_DEBUG("Horizontal Lens Separation: %.3fcm", lens_x_separation * 100.0);
-  GST_DEBUG("Vertical Lens Position: %.3fcm", lens_y_position * 100.0);
+
+  ohmd_device_getf (self->device, OHMD_LENS_HORIZONTAL_SEPARATION,
+      &lens_x_separation);
+  ohmd_device_getf (self->device, OHMD_LENS_VERTICAL_POSITION,
+      &lens_y_position);
+
+  GST_DEBUG ("Horizontal Lens Separation: %.3fcm", lens_x_separation * 100.0);
+  GST_DEBUG ("Vertical Lens Position: %.3fcm", lens_y_position * 100.0);
 
   float left_fov;
   float left_aspect;
   float right_fov;
   float right_aspect;
-  
+
   ohmd_device_getf (self->device, OHMD_LEFT_EYE_FOV, &left_fov);
   ohmd_device_getf (self->device, OHMD_LEFT_EYE_ASPECT_RATIO, &left_aspect);
   ohmd_device_getf (self->device, OHMD_RIGHT_EYE_FOV, &right_fov);
   ohmd_device_getf (self->device, OHMD_RIGHT_EYE_ASPECT_RATIO, &right_aspect);
-  
-  GST_DEBUG("FOV (left/right): %f %f", left_fov, right_fov);
-  GST_DEBUG("Aspect Ratio (left/right): %f %f", left_aspect, right_aspect);
+
+  GST_DEBUG ("FOV (left/right): %f %f", left_fov, right_fov);
+  GST_DEBUG ("Aspect Ratio (left/right): %f %f", left_aspect, right_aspect);
 
   float interpupillary_distance;
   float zfar;
   float znear;
-  
+
   ohmd_device_getf (self->device, OHMD_EYE_IPD, &interpupillary_distance);
-  GST_DEBUG("interpupillary_distance %.3fcm", interpupillary_distance * 100.0);
-  
+  GST_DEBUG ("interpupillary_distance %.3fcm", interpupillary_distance * 100.0);
+
   //self->eye_separation = interpupillary_distance * 100.0 / 2.0;
   self->eye_separation = 0.65;
 
   ohmd_device_getf (self->device, OHMD_PROJECTION_ZNEAR, &znear);
   ohmd_device_getf (self->device, OHMD_PROJECTION_ZFAR, &zfar);
-  
-  GST_DEBUG("znear %f, zfar %f", znear, zfar);  
+
+  GST_DEBUG ("znear %f, zfar %f", znear, zfar);
 
   /*
-  float kappa[6];
-  ohmd_device_getf (self->device, OHMD_DISTORTION_K, kappa);
-  GST_DEBUG("Kappa: %f, %f, %f, %f, %f, %f",
-             kappa[0], kappa[1], kappa[2], kappa[3], kappa[4], kappa[5]);
-  float position[3];
-  ohmd_device_getf (self->device, OHMD_POSITION_VECTOR, position);
-    GST_DEBUG("position: %f, %f, %f",
-             position[0], position[1], position[2]);
+     float kappa[6];
+     ohmd_device_getf (self->device, OHMD_DISTORTION_K, kappa);
+     GST_DEBUG("Kappa: %f, %f, %f, %f, %f, %f",
+     kappa[0], kappa[1], kappa[2], kappa[3], kappa[4], kappa[5]);
+     float position[3];
+     ohmd_device_getf (self->device, OHMD_POSITION_VECTOR, position);
+     GST_DEBUG("position: %f, %f, %f",
+     position[0], position[1], position[2]);
 
-  */
+   */
 }
 
 
@@ -150,12 +163,12 @@ gst_3d_camera_init (Gst3DCamera * self)
   self->device = NULL;
   self->center_distance = 1.0;
   self->scroll_speed = 1.0;
-  
+
   graphene_vec3_init (&self->eye, 0.f, 0.f, 1.f);
   graphene_vec3_init (&self->center, 0.f, 0.f, 0.f);
   graphene_vec3_init (&self->up, 0.f, 1.f, 0.f);
-  
-  init_hmd(self);
+
+  init_hmd (self);
 }
 
 Gst3DCamera *
@@ -170,7 +183,7 @@ gst_3d_camera_finalize (GObject * object)
 {
   Gst3DCamera *self = GST_3D_CAMERA (object);
   g_return_if_fail (self != NULL);
-  
+
   G_OBJECT_CLASS (gst_3d_camera_parent_class)->finalize (object);
 }
 
@@ -181,49 +194,56 @@ gst_3d_camera_class_init (Gst3DCameraClass * klass)
   obj_class->finalize = gst_3d_camera_finalize;
 }
 
-graphene_matrix_t get_hmd_matrix(Gst3DCamera * self, ohmd_float_value type) {
+graphene_matrix_t
+get_hmd_matrix (Gst3DCamera * self, ohmd_float_value type)
+{
   float matrix[16];
   graphene_matrix_t hmd_matrix;
-  
+
   // set hmd rotation, for left eye.
   ohmd_device_getf (self->device, type, matrix);
   graphene_matrix_init_from_float (&hmd_matrix, matrix);
   return hmd_matrix;
-} 
+}
 
-void gst_3d_camera_inc_eye_sep(Gst3DCamera * self) {
+void
+gst_3d_camera_inc_eye_sep (Gst3DCamera * self)
+{
   self->eye_separation += .1;
-  GST_DEBUG("separation: %f", self->eye_separation);
+  GST_DEBUG ("separation: %f", self->eye_separation);
 }
 
-void gst_3d_camera_dec_eye_sep(Gst3DCamera * self) {
+void
+gst_3d_camera_dec_eye_sep (Gst3DCamera * self)
+{
   self->eye_separation -= .1;
-  GST_DEBUG("separation: %f", self->eye_separation);
+  GST_DEBUG ("separation: %f", self->eye_separation);
 }
 
-void gst_3d_camera_update_view_from_quaternion(Gst3DCamera * self) {
-  g_return_if_fail(self->hmd_context);
-  g_return_if_fail(self->device);
+void
+gst_3d_camera_update_view_from_quaternion (Gst3DCamera * self)
+{
+  g_return_if_fail (self->hmd_context);
+  g_return_if_fail (self->device);
 
   ohmd_ctx_update (self->hmd_context);
 
   graphene_matrix_t left_eye_model_view;
-  graphene_matrix_t left_eye_projection = get_hmd_matrix(self, OHMD_LEFT_EYE_GL_PROJECTION_MATRIX);
-  
+  graphene_matrix_t left_eye_projection =
+      get_hmd_matrix (self, OHMD_LEFT_EYE_GL_PROJECTION_MATRIX);
+
 
   graphene_matrix_t right_eye_model_view;
-  graphene_matrix_t right_eye_projection = get_hmd_matrix(self, OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX);
+  graphene_matrix_t right_eye_projection =
+      get_hmd_matrix (self, OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX);
 
   float quaternion[4];
   ohmd_device_getf (self->device, OHMD_ROTATION_QUAT, quaternion);
-  
+
   graphene_quaternion_t quat;
   graphene_quaternion_init (&quat,
-                          quaternion[0],
-                          -quaternion[1],
-                          quaternion[2],
-                          quaternion[3]);
-  
+      quaternion[0], -quaternion[1], quaternion[2], quaternion[3]);
+
   graphene_point3d_t left_eye;
   graphene_point3d_init (&left_eye, +self->eye_separation, 0, 0);
   graphene_matrix_t translate_left;
@@ -233,102 +253,114 @@ void gst_3d_camera_update_view_from_quaternion(Gst3DCamera * self) {
   graphene_point3d_init (&rigth_eye, -self->eye_separation, 0, 0);
   graphene_matrix_t translate_right;
   graphene_matrix_init_translate (&translate_right, &rigth_eye);
-  
+
   graphene_quaternion_to_matrix (&quat, &right_eye_model_view);
-  graphene_matrix_multiply (&right_eye_model_view, &translate_right, &right_eye_model_view);
+  graphene_matrix_multiply (&right_eye_model_view, &translate_right,
+      &right_eye_model_view);
 
   graphene_quaternion_to_matrix (&quat, &left_eye_model_view);
-  graphene_matrix_multiply (&left_eye_model_view, &translate_left, &left_eye_model_view);
-      
+  graphene_matrix_multiply (&left_eye_model_view, &translate_left,
+      &left_eye_model_view);
+
   graphene_matrix_multiply (&left_eye_model_view, &left_eye_projection,
       &self->left_vp_matrix);
-  graphene_matrix_multiply (&right_eye_model_view, &right_eye_projection, 
+  graphene_matrix_multiply (&right_eye_model_view, &right_eye_projection,
       &self->right_vp_matrix);
 }
 
 void
 gst_3d_camera_update_view_from_matrix (Gst3DCamera * self)
 {
-  g_return_if_fail(self->hmd_context);
-  g_return_if_fail(self->device);
+  g_return_if_fail (self->hmd_context);
+  g_return_if_fail (self->device);
 
   ohmd_ctx_update (self->hmd_context);
 
-  graphene_matrix_t left_eye_model_view = get_hmd_matrix(self, OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX);
-  graphene_matrix_t left_eye_projection = get_hmd_matrix(self, OHMD_LEFT_EYE_GL_PROJECTION_MATRIX);
-  
-  graphene_matrix_t right_eye_model_view = get_hmd_matrix(self, OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX);
-  graphene_matrix_t right_eye_projection = get_hmd_matrix(self, OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX);
-      
-  graphene_matrix_multiply (&left_eye_model_view, &left_eye_projection, &self->left_vp_matrix);
-  graphene_matrix_multiply (&right_eye_model_view, &right_eye_projection, &self->right_vp_matrix);
+  graphene_matrix_t left_eye_model_view =
+      get_hmd_matrix (self, OHMD_LEFT_EYE_GL_MODELVIEW_MATRIX);
+  graphene_matrix_t left_eye_projection =
+      get_hmd_matrix (self, OHMD_LEFT_EYE_GL_PROJECTION_MATRIX);
+
+  graphene_matrix_t right_eye_model_view =
+      get_hmd_matrix (self, OHMD_RIGHT_EYE_GL_MODELVIEW_MATRIX);
+  graphene_matrix_t right_eye_projection =
+      get_hmd_matrix (self, OHMD_RIGHT_EYE_GL_PROJECTION_MATRIX);
+
+  graphene_matrix_multiply (&left_eye_model_view, &left_eye_projection,
+      &self->left_vp_matrix);
+  graphene_matrix_multiply (&right_eye_model_view, &right_eye_projection,
+      &self->right_vp_matrix);
 }
 
-void gst_3d_camera_update_view(Gst3DCamera * self) {
+void
+gst_3d_camera_update_view (Gst3DCamera * self)
+{
   gst_3d_camera_update_view_from_quaternion (self);
   // gst_3d_camera_update_view_from_matrix (self);
 }
 
 
-void gst_3d_camera_update_view_mvp(Gst3DCamera * self) {
+void
+gst_3d_camera_update_view_mvp (Gst3DCamera * self)
+{
   graphene_vec3_t eye;
   graphene_vec3_t center;
   graphene_vec3_t up;
   graphene_vec3_init (&eye, 0.f, 0.f, 1.f);
   graphene_vec3_init (&center, 0.f, 0.f, 0.f);
   graphene_vec3_init (&up, 0.f, 1.f, 0.f);
- 
- graphene_matrix_t projection_matrix;
-  graphene_matrix_init_perspective (&projection_matrix,
-      90.0,
-      4.0/3.0, 0.1, 1000.0);
 
- graphene_matrix_t view_matrix;
-  graphene_matrix_init_look_at (&view_matrix, &eye, &center,
-      &up);
+  graphene_matrix_t projection_matrix;
+  graphene_matrix_init_perspective (&projection_matrix,
+      90.0, 4.0 / 3.0, 0.1, 1000.0);
+
+  graphene_matrix_t view_matrix;
+  graphene_matrix_init_look_at (&view_matrix, &eye, &center, &up);
 
   graphene_matrix_multiply (&view_matrix, &projection_matrix, &self->mvp);
 }
 
-void gst_3d_camera_translate_arcball(Gst3DCamera * self, float z) {
-    self->center_distance += z * self->scroll_speed;
-    // updateView();
-    gst_3d_camera_update_view_arcball(self);
+void
+gst_3d_camera_translate_arcball (Gst3DCamera * self, float z)
+{
+  self->center_distance += z * self->scroll_speed;
+  // updateView();
+  gst_3d_camera_update_view_arcball (self);
 }
 
-void gst_3d_camera_update_view_arcball(Gst3DCamera * self) {
+void
+gst_3d_camera_update_view_arcball (Gst3DCamera * self)
+{
   graphene_vec3_t eye;
   graphene_vec3_t center;
   graphene_vec3_t up;
-  
-  float radius = exp(-self->center_distance);
-  
+
+  float radius = exp (-self->center_distance);
+
   /*
-  glm::vec3 center(0, 0, 0);
-    glm::vec3 up(0, 0, -1);
+     glm::vec3 center(0, 0, 0);
+     glm::vec3 up(0, 0, -1);
 
-    glm::vec3 eye(radius * sin(theta) * cos(phi),
-                  radius * sin(theta) * sin(phi),
-                  radius * cos(theta));
+     glm::vec3 eye(radius * sin(theta) * cos(phi),
+     radius * sin(theta) * sin(phi),
+     radius * cos(theta));
 
-    // printf("theta %f phi %f scrollPosition %f\n",
-    //   theta, phi, scrollPosition);
+     // printf("theta %f phi %f scrollPosition %f\n",
+     //   theta, phi, scrollPosition);
 
-    view = glm::lookAt(eye, center, up);
-  */
-  
+     view = glm::lookAt(eye, center, up);
+   */
+
   graphene_vec3_init (&center, 0.f, 0.f, 0.f);
   graphene_vec3_init (&up, 0.f, 0.f, -1.f);
-  graphene_vec3_init (&eye, radius * sin(self->theta) * cos(self->phi),
-                            radius * sin(self->theta) * sin(self->phi),
-                            radius * cos(self->theta));
- 
- graphene_matrix_t projection_matrix;
-  graphene_matrix_init_perspective (&projection_matrix,
-      90.0,
-      4.0/3.0, 0.1, 1000.0);
+  graphene_vec3_init (&eye, radius * sin (self->theta) * cos (self->phi),
+      radius * sin (self->theta) * sin (self->phi), radius * cos (self->theta));
 
- graphene_matrix_t view_matrix;
+  graphene_matrix_t projection_matrix;
+  graphene_matrix_init_perspective (&projection_matrix,
+      90.0, 4.0 / 3.0, 0.1, 1000.0);
+
+  graphene_matrix_t view_matrix;
   graphene_matrix_init_look_at (&view_matrix, &eye, &center, &up);
 
   graphene_matrix_multiply (&view_matrix, &projection_matrix, &self->mvp);
